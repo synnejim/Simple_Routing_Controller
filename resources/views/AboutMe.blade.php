da573a097d3ff58d9ac5b2adf5f8bfdb1da2ca55
synnejim <navaltasynnejim@gmail.com>
2024-06-22 22:16:25 +0800

    Created views and controller

---
+++ resources/views/AboutMe.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+    <p>I'm Synne Jim S. Na
+</div>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about-me', function () {
+    return view('AboutMe');
+});
+
+Route::get('/skills', function () {
+    return view('Skills');
+});
+
+Route::get('/hobbies', function () {
+    return view('Hobbies');
+});
